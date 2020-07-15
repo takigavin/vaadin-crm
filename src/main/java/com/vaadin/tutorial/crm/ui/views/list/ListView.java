b03df2d9fdb81d2d62c499f620cabb105861e94c
takigavin <gavinhorner0@gmail.com>
2020-07-15 18:18:35 -0500

    added PWA functionality and unit and integration testing capabilities

--- src/main/java/com/vaadin/tutorial/crm/ui/views/list/ListView.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/views/list/ListView.java
@@ -14,15 +14,17 @@ import com.vaadin.tutorial.crm.backend.entity.Contact;
 import com.vaadin.tutorial.crm.backend.service.CompanyService;
 import com.vaadin.tutorial.crm.backend.service.ContactService;
 import com.vaadin.tutorial.crm.ui.MainLayout;
+import org.springframework.stereotype.Component;
 
+@Component
 @Route(value = "", layout = MainLayout.class)
 @PageTitle("Contacts | Vaadin CRM")
 public class ListView extends VerticalLayout {
 
-    private final ContactForm form;
+    ContactForm form;
     Grid<Contact> grid = new Grid<>(Contact.class);
     TextField filterText = new TextField();
-    private ContactService contactService;
+    ContactService contactService;
 
     public ListView(ContactService contactService, CompanyService companyService) {
         this.contactService = contactService;
